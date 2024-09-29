bd50f3eb76ce782199a077a93390514bb94bdbc2
Max <desiighn3r@gmail.com>
2024-09-29 22:55:56 +0300

    Add tag

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+import random
+
+def fill_array_with_random_values(size):
+    return [random.randint(1, 100) for _ in range(size)]
+
+def print_array(array):
+    print("Массив:", array)
+
+def main():
+    size = int(input("Введите размер массива: "))
+    array = fill_array_with_random_values(size)
+    print_array(array)
+
+if __name__ == "__main__":
+    main()
